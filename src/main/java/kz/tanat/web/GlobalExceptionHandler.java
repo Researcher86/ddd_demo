e3add1136fb41ff858f7a08ce4b7397ea9fbaa10
Tanat <researcher2286@gmail.com>
2017-07-15 16:13:12 +0600

    Добавить глобальный обработчик сообщений об ошибках

---
+++ src/main/java/kz/tanat/web/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package kz.tanat.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Глобальный обработчик сообщений об ошибках.
+ *
+ * @author Tanat
+ * @since 15.07.2017.
+ */
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not found")
+    public void exceptionHandler404(IllegalArgumentException e) {
+        log.error("REST api exception", e);
+    }
+}
