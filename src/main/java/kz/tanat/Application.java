4e688ac34568a340b6ce9a72bd90ac2d0e437472
Tanat <researcher2286@gmail.com>
2017-07-15 16:12:31 +0600

    Добавить библиотеку для генерации REST API документацию

--- src/main/java/kz/tanat/Application.java
+++ src/main/java/kz/tanat/Application.java
@@ -9,22 +9,50 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.UUID;
+
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 /**
  * Класс для запуска системы.
  *
  * @author Tanat
- * @version 1.1
  * @since 07.07.2017.
  */
 @Slf4j
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(SWAGGER_2)
+                .groupName("employees")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("kz.tanat.web"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Employee REST API")
+                .contact(new Contact("Alpenov Tanat",
+                        "https://github.com/Batiaev/demo-quotation-webservice",
+                        "researcher2286@gmail.com"))
+                .version("1.0")
+                .build();
     }
 
     @Autowired
