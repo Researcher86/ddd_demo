85317ff30fa4d1b1fc6b89dc841c94d295c7929e
Tanat <researcher2286@gmail.com>
2017-08-12 18:08:38 +0600

    Добавить монитор всех событий системы

---
+++ src/main/java/kz/tanat/app/DomainEventMonitor.java
@@ -0,0 +1,44 @@
+package kz.tanat.app;
+
+import kz.tanat.domain.DomainEvent;
+import kz.tanat.domain.DomainEventPublisher;
+import kz.tanat.domain.DomainEventSubscriber;
+import kz.tanat.domain.event.EventRepository;
+import kz.tanat.domain.event.StoredEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Класс перехватывает вызовы к сервисам приложения и регистрирует слушателя для перехвата всех событий системы.
+ *
+ * @author Tanat
+ * @since 12.08.2017.
+ */
+@Slf4j
+@Aspect
+@Component
+public class DomainEventMonitor {
+	private final EventRepository eventRepository;
+
+	public DomainEventMonitor(EventRepository eventRepository) {
+		this.eventRepository = eventRepository;
+	}
+
+	@Around("execution(* kz.tanat.app..*Service.*(..))")
+	public Object webServiceMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+		DomainEventPublisher.instance().reset();
+
+		DomainEventPublisher.instance().subscribe((DomainEventSubscriber<DomainEvent>) domainEvent -> {
+			StoredEvent storedEvent = new StoredEvent(domainEvent);
+
+			log.info("Event: {}", storedEvent.eventBody());
+
+			eventRepository.save(storedEvent);
+		});
+
+		return joinPoint.proceed();
+	}
+}
