b7b934a02efdec902be8c70e65666a48261476f5
Tanat <researcher2286@gmail.com>
2017-08-20 16:47:32 +0600

    Refactor

--- src/main/java/kz/tanat/web/controller/EmployeeController.java
+++ src/main/java/kz/tanat/web/controller/EmployeeController.java
@@ -1,14 +1,11 @@
 package kz.tanat.web.controller;
 
 import kz.tanat.app.employee.EmployeeService;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
 
 /**
  * Web контроллер для Employee.
@@ -33,12 +30,6 @@ public class EmployeeController {
 
     @GetMapping("/{id}")
     public ModelAndView show(@PathVariable String id) {
-        try {
-            return new ModelAndView("employee/show", "employee", employeeService.get(id));
-        } catch (IllegalArgumentException e) {
-            return new ModelAndView("error/404", new HashMap<String, String>() {{
-                put("error", e.getMessage());
-            }}, HttpStatus.NOT_FOUND);
-        }
+        return new ModelAndView("employee/show", "employee", employeeService.get(id));
     }
 }
